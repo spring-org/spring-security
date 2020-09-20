a200cefb4158f59d2aa13ef4f919b1909b7d6d3c
seokrae <kslbsh@gmail.com>
2020-09-20 20:50:40 +0900

    2. security-basic: Custom Security

---
+++ security-basic-api/src/main/java/kr/seok/SecurityConfig.java
@@ -0,0 +1,22 @@
+package kr.seok;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 인가 정책
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+
+        // 인증 정책
+        http
+                .formLogin();
+    }
+}
---
+++ security-basic-api/src/main/java/kr/seok/controller/BasicController.java
@@ -0,0 +1,13 @@
+package kr.seok.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
