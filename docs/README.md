74b26abb2b818a9affe8d22b37dc71cb238f5a80
seokrae <kslbsh@gmail.com>
2020-09-25 09:41:55 +0900

    2.1.8 RememberMe

--- docs/README.md
+++ docs/README.md
@@ -1,8 +1,10 @@
 # Table of Contents
 
 ## 1. 강의 소개
+[intro](/docs/lecture/1.intro.md)
 
 ## 2. 스프링 시큐리티 기본 API & Filter 이해
+[security-basic](/docs/lecture/2.security-basic.md)
 
 ## 3. 시큐리티 주요 아키텍처 이해 
 
--- security-basic-api/src/main/java/kr/seok/SecurityConfig.java
+++ security-basic-api/src/main/java/kr/seok/SecurityConfig.java
@@ -1,25 +1,21 @@
 package kr.seok;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 인가 정책
@@ -30,47 +26,82 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .formLogin()
 //                .loginPage("/loginPage")
-                .defaultSuccessUrl("/")
-                .failureUrl("/login")
-                .usernameParameter("userId")
-                .passwordParameter("passwd")
-                .loginProcessingUrl("/login_proc")
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        System.out.println("authentication : " + authentication.getName());
-                        response.sendRedirect("/");
-                    }
-                })
-                .failureHandler(new AuthenticationFailureHandler() {
-                    @Override
-                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                        System.out.println("exception" + exception.getMessage());
-                        response.sendRedirect("/login");
-                    }
-                })
-                .permitAll();
-
-        http
-                .logout()
-                /* GET, POST 가능 */
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .addLogoutHandler(new LogoutHandler() {
-                    @Override
-                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-                        HttpSession session = request.getSession();
-                        session.invalidate();
-                    }
-                })
-                .logoutSuccessHandler(new LogoutSuccessHandler() {
-                    @Override
-                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        response.sendRedirect("/login");
-                    }
-                })
-                /* 쿠키명 */
-                .deleteCookies("remember-me")
-        ;
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/login")
+//                .usernameParameter("userId")
+//                .passwordParameter("passwd")
+//                .loginProcessingUrl("/login_proc")
+//                .successHandler(new AuthenticationSuccessHandler() {
+//                    @Override
+//                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//                        System.out.println("authentication : " + authentication.getName());
+//                        response.sendRedirect("/");
+//                    }
+//                })
+//                .failureHandler(new AuthenticationFailureHandler() {
+//                    @Override
+//                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+//                        System.out.println("exception" + exception.getMessage());
+//                        response.sendRedirect("/login");
+//                    }
+//                })
+//                .permitAll()
+//            .and()
+//                .logout()
+//                /* GET, POST 가능 */
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/login")
+//                .addLogoutHandler(new LogoutHandler() {
+//                    @Override
+//                    public void logout(
+//                            HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+//                        HttpSession session = request.getSession();
+//                        session.invalidate();
+//                    }
+//                })
+//                .logoutSuccessHandler(new LogoutSuccessHandler() {
+//                    @Override
+//                    public void onLogoutSuccess(
+//                            HttpServletRequest request, HttpServletResponse response, Authentication authentication
+//                    ) throws IOException, ServletException {
+//                        response.sendRedirect("/login");
+//                    }
+//                })
+//                /* 쿠키명 */
+//                .deleteCookies("remember-me")
+            .and()
+                .rememberMe()
+//                .rememberMeParameter("remember") // 기본 파라미터 명 -> "remember-me"
+//                .tokenValiditySeconds(3600) // 만료시간 default 14일
+//                .alwaysRemember(true) // remember me  기능이 활성화되지 않아도 항상 실행
+                /* user 계정 확인 메서드 */
+                .userDetailsService(userDetailsService)
+                ;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
