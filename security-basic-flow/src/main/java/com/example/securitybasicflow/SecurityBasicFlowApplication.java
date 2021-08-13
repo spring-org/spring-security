121bc827bb263e7c5d41c8004ae4d6e1fae50f1e
seokrae <kslbsh@gmail.com>
2021-08-13 16:26:27 +0900

    feat: 시큐리티 설정 및 기본 필터 확인

---
+++ security-basic-flow/src/main/java/com/example/securitybasicflow/SecurityBasicFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitybasicflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBasicFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityBasicFlowApplication.class, args);
+    }
+
+}
---
+++ security-basic-flow/src/test/java/com/example/securitybasicflow/SecurityBasicFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitybasicflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBasicFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
